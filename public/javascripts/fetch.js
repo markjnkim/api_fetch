f85d91b8b97902669d8a34be50a82aa22a173a8a
trumpocalyspe <mark.jn.kim@gmail.com>
2018-04-26 12:16:47 -0700

    fetches flag images by correcting path in flag.min.css

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -1,7 +1,14 @@
-fetch('http://example.com/movies.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  });
+fetch('https://randomuser.me/api/?results=100').then((response) => {
+            return response.json();
+        }).then((myJSON) => {
+            let html = '';
+            for (const result of myJSON.results) {
+                html = `${html}
+                    <div class="person">
+                        <img align="middle" src="${result.picture.medium}" alt="Profile Picture">
+                        <h3><span class="flag-icon flag-icon-${result.nat.toLowerCase()}"></span> ${result.name.title} ${result.name.first} ${result.name.last}</h3>
+                    </div>
+                `;
+            }
+            document.getElementById('people').innerHTML = html;
+        });
